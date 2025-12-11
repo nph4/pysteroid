012740d8f8cc35f51f4dacb3434bcd963a1538bb
Nelson Hickman <nelson.hickman@gmail.com>
2025-12-10 16:11:15 -0800

    end of chapter 2

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -16,6 +18,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        #limit the framerate to 60fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
